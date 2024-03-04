2c4c3a6aa6b25d7a540fcd879902ac80cece7f16
kumar-peeyush <kumarpeeyush551@gmmail.com>
2024-03-04 17:48:52 +0530

    creating repo

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
